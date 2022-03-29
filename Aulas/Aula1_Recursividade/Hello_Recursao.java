0db13166fa6a1797f59bb51d2db936d828af3bd1
thiago-igor <thiago.maia0606@gmail.com>
2022-03-29 16:14:35 -0300

    atualizaçao aula 1

---
+++ Aulas/Aula1_Recursividade/Hello_Recursao.java
@@ -0,0 +1,41 @@
+package Aulas.Aula1_Recursividade;
+
+public class Hello_Recursao {
+    public static void main(String[] args) {
+        decremento(4);
+        //System.out.println(fatorial(3));
+    }
+
+    public static void incrementoDecremento(int numero) {
+        if (numero > 0) {
+            System.out.println(numero);
+            incrementoDecremento(numero - 1);
+        }
+        System.out.println(numero);
+    }
+
+    public static void incremento(int numero) {
+        if (numero > 0) {
+            incremento(numero - 1);
+            System.out.println(numero);
+        } else {
+            System.out.println(numero);
+        }
+    }
+
+    public static void decremento(int numero) {
+        if (numero > 0) {
+            System.out.println(numero);
+            decremento(numero - 1);
+        } else {
+            System.out.println(numero);
+        }
+    }
+
+    public static int fatorial(int numero) {
+        if (numero > 1) {
+            return numero * fatorial(--numero);
+        }
+        return numero;
+    }
+}
