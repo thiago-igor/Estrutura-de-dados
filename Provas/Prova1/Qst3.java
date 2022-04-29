756087133a50fae9bab94b52b999c1024585a6ff
thiago-igor <thiago.maia0606@gmail.com>
2022-04-29 16:45:02 -0300

    atualizaçao

--- Provas/Prova1/Qst3.java
+++ Provas/Prova1/Qst3.java
@@ -3,6 +3,24 @@ package Provas.Prova1;
 
 public class Qst3 {
     public static void main(String[] args){
+       int num = 5;
+
+       for(int i=0;i<num;i++){
+           System.out.println(fibonacciRecursivo(i));
+       }
         
     }
+
+    public static int fibonacciRecursivo(int num){
+        if (num <2){
+            return 1;
+        }
+        return fibonacciRecursivo(num-1) + fibonacciRecursivo(num-2);
+    }
+
+
+
+
+
+
 }
