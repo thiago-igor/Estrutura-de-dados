a7e1a4b7dd301a19fa47b01813bc99e56f518518
thiago-igor <thiago.maia0606@gmail.com>
2022-05-14 16:21:57 -0300

    atualizaçao lista duplamente encadeada

--- Aulas/ListaDuplamenteEncadeada/forma2/ListaDupla.java
+++ Aulas/ListaDuplamenteEncadeada/forma2/ListaDupla.java
@@ -5,6 +5,10 @@ public class ListaDupla {
     No fim;
     int tamanho;
 
+    public static void main(String[] args) {
+        
+    }
+
     public void inserirInicio(String info){
         No no = new No();
         no.info = info;
@@ -64,9 +68,59 @@ public class ListaDupla {
         tamanho--;
         return out;
     }
-    
 
+    public void inserirMeio(int indice, String info){
+        if( indice <= 0){
+            inserirInicio(info);
+        }else if (indice >= tamanho){
+            inserirFim(info);
+        }else{
+            No local = inicio;
+            for(int i = 0; i<indice-1;i++){
+                local = local.proximo;
+            }
+            No no = new No();
+            no.info= info;
+            no.anterior = local;
+            no.proximo = local.proximo;
+            local.proximo = no;
+            no.proximo.anterior = no;
+            tamanho++;
+        }
+    }
 
-      
+    public String retirarMeio(int indice){
+        if(indice < 0 || indice>=tamanho || inicio == null ){
+            return null;
+
+        }else if(indice == 0){
+            retirarInicio();
+
+        }else if(indice == tamanho-1){
+            retiraFim();
+        }
+        No local = inicio;
+        for(int i = 0; i<indice;i++){
+            local = local.proximo;
+        }
+        if(local.anterior != null){
+            local.anterior.proximo = local.proximo;
+        }
+        if(local.proximo != null){
+            local.proximo.anterior = local.anterior;
+        }
+        tamanho--;
+        return local.info;
+    }
+
+    public String toString(){
+        String str = "(" + tamanho + ") ";
+        No local = inicio;
+        while(local != null){
+            str += local.info + " ";
+            local = local.proximo;
+        }
+        return str;
+    }
 
 }
