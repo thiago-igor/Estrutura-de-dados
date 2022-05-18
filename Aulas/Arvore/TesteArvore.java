ea3b3b5a8e024a9c74e49d2f9346f4bba65c4df5
thiago-igor <thiago.maia0606@gmail.com>
2022-05-18 20:42:16 -0300

    atualizaçao

--- Aulas/Arvore/TesteArvore.java
+++ Aulas/Arvore/TesteArvore.java
@@ -1,9 +1,16 @@
 package Aulas.Arvore;
 
 public class TesteArvore {
-    public void main(String[] args) {
+
+    public static void main(String[] args) {
         ArvoreBinaria ab = new ArvoreBinaria(20);
         ab.adicionaNo(new No(30), ab.getArvoreBinaria());
+        ab.adicionaNo(new No(60), ab.getArvoreBinaria());
+        ab.adicionaNo(new No(25), ab.getArvoreBinaria());
+        ab.adicionaNo(new No(40), ab.getArvoreBinaria());
+        ab.adicionaNo(new No(65), ab.getArvoreBinaria());
+
+        ab.imprimirEmOrdem(ab.getArvoreBinaria());
         
     }
 }
