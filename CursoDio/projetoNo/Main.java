c221095bbb76f49527488508d4fc92e2f20aac96
thiago-igor <thiago.maia0606@gmail.com>
2022-06-17 20:48:22 -0300

    introdução a no

---
+++ CursoDio/projetoNo/Main.java
@@ -0,0 +1,21 @@
+package CursoDio.projetoNo;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        No no1 = new No("conteudo no1");
+
+        No no2 = new No("conteudo no2");
+        no1.setProximoNo(no2);
+
+        No no3 = new No("conteudo no3");
+        no2.setProximoNo(no3);
+
+        //No1->No2->No3-> null
+
+        System.out.println(no1);
+        System.out.println(no1.getProximoNo()); // acessando o No2
+        System.out.println(no2);
+        System.out.println(no3);
+    }
+}
